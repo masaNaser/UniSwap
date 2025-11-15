b2a83006394388331323e06360a26c2cafd8fe3e
Masanaser <masaNaser@users.noreply.github.com>
2025-11-15 17:16:31 +0200

    Add Logo.png

--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -17,7 +17,7 @@ import ChatBubbleOutlineOutlinedIcon from "@mui/icons-material/ChatBubbleOutline
 import WorkspacePremiumOutlinedIcon from "@mui/icons-material/WorkspacePremiumOutlined";
 import ShieldOutlinedIcon from "@mui/icons-material/ShieldOutlined";
 import { ElectricBoltSharp } from "@mui/icons-material";
-import Logo from "../../../../assets/images/Logo.png";
+import Logo from "../../assets/images/Logo.png";
 import heroImg from "../../assets/images/hero-bg.png";
 import MenuIcon from "@mui/icons-material/Menu";
 
